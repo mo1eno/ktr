c0e3e3baaa0643357540c7ab22d192db770ccc25
mo1eno <rutopoyrar@gmail.com>
2023-10-10 10:40:09 +1000

    ver1

---
+++ src/Components/DishItem.js
@@ -0,0 +1,10 @@
+const DishItem = () => {
+  const data = [1, 2, 3];
+  ItemDish(data);
+};
+
+const ItemDish = (data) => {
+  const variable = data;
+  console.log(variable);
+};
+export default DishItem;
